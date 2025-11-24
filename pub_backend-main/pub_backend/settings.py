06e51d89b5d29674f2e83b510db4c20fb31710f3
jungam5222 <jungam5222@naver.com>
2025-11-24 20:17:44 +0900

    railway setting

--- pub_backend-main/pub_backend/settings.py
+++ pub_backend-main/pub_backend/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -88,6 +89,9 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
